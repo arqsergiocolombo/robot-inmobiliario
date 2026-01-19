b8a857c264c61dd8a4acafb022a9f9ce70fa3ba9
arqsergiocolombo <arq.sergiocolombo@gmail.com>
2026-01-19 12:18:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 import scraper_ml
-import sheets
-import time
-
-def run():
-    print("--- 🤖 Robot Inmobiliario v2.0 (Argenprop) ---")
-    
-    # 1. Scrapear
-    propiedades = scraper_ml.scrape_all()
-    
-    # 2. Exportar si hay éxito
-    if propiedades:
-        sheets.export_to_sheets(propiedades)
-    else:
-        print("❌ El proceso terminó sin encontrar datos nuevos.")
-    
-    print("--- 🏁 Fin del proceso ---")
 
 if __name__ == "__main__":
-    run()
+    print("Iniciando Robot...")
+    scraper_ml.scrape_all()
